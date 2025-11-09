718b4d1fe3b672e5f04871a1cb0d96b2a22dc43a
DangBao221005 <dangbaonguyen005@gmail.com>
2025-11-09 14:06:52 +0700

    Bảo (Cập Nhật file code 9/11)

--- src/main/java/vn/DrinkOrder/Module_Payment/ModulePaymentApplication.java
+++ src/main/java/vn/DrinkOrder/Module_Payment/ModulePaymentApplication.java
@@ -2,12 +2,13 @@ package vn.DrinkOrder.Module_Payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ModulePaymentApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ModulePaymentApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Thiết lập múi giờ mặc định cho toàn bộ ứng dụng Spring Boot
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SpringApplication.run(ModulePaymentApplication.class, args);
+    }
 }
